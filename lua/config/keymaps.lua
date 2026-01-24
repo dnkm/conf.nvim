a40b4420ae966a5b33660c7c50465bf41910d83f
Daniel Kim <chipperday@gmail.com>
2026-01-24 11:08:53 -0800

    DK run polish

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,7 @@ vim.keymap.set("n", "<leader>cx", function()
   local file = vim.fn.expand("%")
   local root = vim.fn.expand("%:r") -- without extension
   local ft = vim.bo.filetype
+  local buf_name = "SmartRun_Output"
 
   local function with_input(base_cmd)
     if vim.fn.filereadable(root .. ".in") == 0 then
@@ -25,6 +26,18 @@ vim.keymap.set("n", "<leader>cx", function()
     return
   end
 
-  -- vertically split terminal
-  vim.cmd("vsplit | terminal " .. with_input(cmd))
+  -- find and close existing output window/buffer
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_get_name(buf):find(buf_name) then
+      vim.api.nvim_buf_delete(buf, { force = true })
+    end
+  end
+
+  -- open a new split for output
+  vim.cmd("botright vsplit")
+  vim.cmd("terminal " .. with_input(cmd))
+  local new_buf = vim.api.nvim_get_current_buf()
+  vim.api.nvim_buf_set_name(new_buf, buf_name)
+  vim.bo[new_buf].buflisted = false
+  vim.cmd("startinsert")
 end, { desc = "DK: Compile and run C++ file With INPUT" })
