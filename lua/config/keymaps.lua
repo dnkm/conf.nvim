1d5d0e2fb4a60498e258fa9d81e9b7d5c337c1e0
Daniel Kim <chipperday@gmail.com>
2026-01-24 10:23:55 -0800

    auto execute c++ and python with input support

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,15 +3,28 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>cx", function()
   local file = vim.fn.expand("%")
-  local output = vim.fn.expand("%:r") -- without extension
-  local cmd = string.format("g++ %s -o %s && %s", file, output, output)
-  vim.cmd("vsplit | terminal " .. cmd)
-end, { desc = "Compile and run C++ file" })
+  local root = vim.fn.expand("%:r") -- without extension
+  local ft = vim.bo.filetype
 
-vim.keymap.set("n", "<leader>cX", function()
-  local file = vim.fn.expand("%")
-  local output = vim.fn.expand("%:r") -- without extension
-  local cmd = string.format("g++ %s -o %s && %s < %s.in", file, output, output, output)
+  local function with_input(base_cmd)
+    if vim.fn.filereadable(root .. ".in") == 0 then
+      -- vim.notify("input file not found: " .. root .. ".in", vim.log.levels.WARN)
+      return base_cmd
+    end
+    -- vim.notify("Using input file: " .. root .. ".in", vim.log.levels.INFO)
+    return string.format("%s < %s.in", base_cmd, root)
+  end
+
+  local cmd = ""
+  if ft == "cpp" then
+    cmd = string.format("g++ %s -o %s && %s", file, root, root)
+  elseif ft == "python" then
+    cmd = string.format("python3 %s", file)
+  else
+    vim.notify("Unsupported file type for compilation with input", vim.log.levels.ERROR)
+    return
+  end
+
   -- vertically split terminal
-  vim.cmd("vsplit | terminal " .. cmd)
-end, { desc = "Compile and run C++ file With INPUT" })
+  vim.cmd("vsplit | terminal " .. with_input(cmd))
+end, { desc = "DK: Compile and run C++ file With INPUT" })
